f21a604c73ccbefae4e15429589d10c0e48bc2d6
RobloxNationGaming <77547237+RobloxNationGaming@users.noreply.github.com>
2021-03-25 18:39:14 +0800

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
     let invite = new Discord.MessageEmbed()
     .setTitle("Invite & Support Link!")
     .addField("Invite Link", "[Click here to invite me](https://bit.ly/InviteZeroMusic)")
-    .addField("Support Server", "[Click to join support Server](https://discord.gg/REAW5VM)")
+    .addField("Support Server", "[Click to join support Server](https://discord.gg/AsK777PDSw)")
     .setTimestamp()
     .setFooter(`Requested by ${message.author.tag}`, client.user.displayAvatarURL())
     message.channel.send(invite);
